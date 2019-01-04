e3a004513d48eb508be84dbe666e9816a17bd8e4
Rubén Soler <r.solerginer@gmail.com>
2019-01-04 10:43:04 +0100

    Feature persist blacklist

--- Store.js
+++ Store.js
@@ -8,7 +8,8 @@ import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
   key: 'root',
-  storage
+  storage,
+  blacklist: ['selectedMovie']
 }
 
 const persistedReducer = persistReducer(persistConfig, reducer)
